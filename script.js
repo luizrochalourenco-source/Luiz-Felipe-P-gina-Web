0d232e58299b8a2d91e19bda7972482348bb3d06
luizrochalourenco-source <luiz.rocha.lourenco@escola.pr.gov.br>
2026-08-17 00:47:10 -0300

    teste

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const button = document.getElementById("tema");
-button.addEventListener("click",function name(params) {
+button.addEventListener("click", function name(params) {
     
 } () {
     document.body.classList.toggle("dark");
