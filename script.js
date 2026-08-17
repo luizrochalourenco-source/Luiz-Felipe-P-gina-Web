30d524125ef2525368a9e10f04c8a903fc4f2c97
luizrochalourenco-source <luiz.rocha.lourenco@escola.pr.gov.br>
2026-08-17 01:53:33 -0300

    teste

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const botao = document.getElementById('btnMudarCor');
 
 botao.addEventListener('click', () => {
-  // Verifica a cor atual e alterna entre branco e azul
-  if (document.body.style.backgroundColor === 'rgb(0, 123, 255)' || document.body.style.backgroundColor === 'blue') {
-    document.body.style.backgroundColor = '#ffffff'; // Voltar para branco
-  } else {
-    document.body.style.backgroundColor = '#007bff'; // Mudar para azul
-  }
+  // O classList.toggle adiciona a classe se ela não existir, e remove se existir
+  document.body.classList.toggle('fundo-azul');
 });
